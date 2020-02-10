252f3a0d28297dd128eeaa38dc6c8925e87a46d1
Mithun Kumer Ghose <mkumerg@gmail.com>
2020-02-10 17:55:37 +0600

    Completed predicate consumer and some other predefined functional interface

--- FirstModule/src/com/mithun/MyModuleFile.java
+++
@@ -1,5 +0,0 @@
-package com.mithun;
-
-public class MyModuleFile {
-
-}
--- FirstModule/src/com/mithun/prac1/Practice1.java
+++ FirstModule/src/com/mithun/prac1/Practice1.java
@@ -1,6 +1,9 @@
 package com.mithun.prac1;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Practice1 {
 
@@ -33,17 +36,47 @@ public class Practice1 {
         //Converting temp from cel to farh
         Function<Double, Double> celToFarh = cel -> cel * 9.0 / 5.0 + cel;
 
-        System.out.println("The Area of a Square with length 3 is : " + areaOfSquare.apply(3.0));
-        System.out.println("The celsius equivalent of 60 deg farh is : " + farhToCelsius.apply(60.0));
-        System.out.println("The farh equivalent of 60 deg farh is : " + celToFarh.apply(60.0));
+//        System.out.println("The Area of a Square with length 3 is : " + areaOfSquare.apply(3.0));
+//        System.out.println("The celsius equivalent of 60 deg farh is : " + farhToCelsius.apply(60.0));
+//        System.out.println("The farh equivalent of 60 deg farh is : " + celToFarh.apply(60.0));
 
         //Function andThen demo
         Function<String, String> fun1 = String::toUpperCase;
         Function<String, String> fun2 = s -> s + " World";
 
         //Output of fun1
-        System.out.println("Output of fun1 " + fun1.apply("Hello"));
+        /*System.out.println("Output of fun1 " + fun1.apply("Hello"));
         //Output of andThen
         System.out.println("Output of andThen " + fun1.andThen(fun2).apply("Hello"));
+        System.out.println("Output of andThen " + fun1.compose(fun2).apply("Hello"));
+        */
+        //Predicate Demo
+        Predicate<Integer> pre = val -> val < 10;
+        if(pre.test(5))
+            System.out.println("The value is less than 10");
+        else
+            System.out.println("The value is greater that 10");
+
+        //supplier demo
+        Supplier<Double> bdtRate = () -> {
+            double bdt = 84.59;//You can call webservice or some other task here.
+            return bdt;
+        };
+        System.out.println("The USD to BDT rate is : " + bdtRate.get());
+
+        //Consumer Demo
+        Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
+        consumer.accept("mithun Kumer ghose");
+        Consumer<String> con = MessageUtil::sendMessage;
+        con.accept("Account has been credited");
+
+    }
+}
+
+class MessageUtil {
+    public static void sendMessage(String message) {
+        //do connection to the message server
+        //create exact message from the message object
+        System.out.println("Your message has been sent successfully.\n" + message);
     }
 }
